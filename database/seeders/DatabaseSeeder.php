3928286f6494e7d454801fe4a4a727cb5ed9171b
Youcef <h.youcef6346@gmail.com>
2024-11-14 18:08:18 +0000

    Add call to seeders in DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Package;
+use App\Models\Store;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
     /**
      * Seed the application's database.
      */
@@ -16,8 +20,30 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+        ]);
+
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            WilayaSeeder::class,
+            CommuneSeeder::class,
+            DeliveryTypeSeeder::class,
+            PackageStatusSeeder::class,
+        ]);
+
+        $stores = Store::factory()->count(5000)->create();
+        $chunks = $stores->chunk(1000);
+        $chunks->each(function($chunk) {
+            foreach ($chunk as $store) {
+                $packages = Package::factory()->count(100)->create([
+                    'store_id' => $store['id']
+                ]);
+            }
+        });
     }
 }
